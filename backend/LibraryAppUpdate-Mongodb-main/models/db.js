37e2a2f8a90a6d3bf626819c4135aad480d9808b
SajidMarakkar <72130333+saajid-marakkar@users.noreply.github.com>
2021-04-20 18:07:14 +0530

    initial

--- backend/LibraryAppUpdate-Mongodb-main/models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose =  require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/libbapp',{useNewUrlParser: true},(err)=> {
-    if(!err){console.log("database created")}
-    else{console.log("error detected" + err)}
-})
-
-require('../models/libschema') 
--- backend/LibraryAppUpdate-Mongodb-main/models/libschema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-var bookschema = new mongoose.Schema({
-    bookName: String,
-    authorName: String,
-    image:String
-});
-
-mongoose.model('addbooks', bookschema)
